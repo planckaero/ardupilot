d8353796a4e6f9c07e3af6b3561a3714b2b3a9ef
CraftyJax <ajaff001@gmail.com>
2021-06-07 09:06:13 -0700

    move motor led to AP_Notify

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -257,6 +257,22 @@ void Copter::init_ardupilot()
 
     hal.console->printf("\nReady to FLY ");
 
+
+//    AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev1 =  std::move(hal.i2c_mgr->get_device(0, 0x51));
+//    dev1->write_register(0x0a,127);
+//    dev1->write_register(0x0b,2);
+
+//    AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev2 =  hal.i2c_mgr->get_device(0, 0x52);
+//    dev2->write_register(0x0a,127);
+//    dev2->write_register(0x0b,2);
+
+//    AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev3 =  hal.i2c_mgr->get_device(0, 0x53);
+//    dev3->write_register(0x0a,127);
+//    dev3->write_register(0x0b,2);
+
+//    AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev4 =  hal.i2c_mgr->get_device(0, 0x54);
+//    dev4->write_register(0x0a,127);
+//    dev4->write_register(0x0b,2);
     // flag that initialisation has completed
     ap.initialised = true;
 
